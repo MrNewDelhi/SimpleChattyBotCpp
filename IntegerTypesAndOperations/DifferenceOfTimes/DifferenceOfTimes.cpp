baec87bc7a808e8251a832a2c475f508a34b2c92
anmol <anmolsoin1@gmail.com>
2022-01-26 16:06:19 +0530

    ProjectWork

--- IntegerTypesAndOperations/DifferenceOfTimes/DifferenceOfTimes.cpp
+++ IntegerTypesAndOperations/DifferenceOfTimes/DifferenceOfTimes.cpp
@@ -12,7 +12,15 @@ int main(){
     int min2 {0};
     int sec2 {0};
 
-    cin >> hour1 >> min1 >> sec1 >> hour2 >> min2 >> sec2;
+    cin >> hour1 >> min1 >> sec1;
+    cin >> hour2 >> min2 >> sec2;
 
-    
+    int hour_to_min1 = hour1 * 60 + min1;
+    int hour_to_min2 = hour2 * 60 + min2;
+
+    int min_to_sec1 = hour_to_min1 * 60 + sec1;
+    int min_to_sec2 = hour_to_min2 * 60 + sec2;
+
+    int diff = min_to_sec2 - min_to_sec1;
+    cout << diff;
 }
