ab5df7cd874919fcbebcf7dd99ef58301f78749e
anmol <anmolsoin1@gmail.com>
2022-01-27 19:47:02 +0530

    ProjectWork

---
+++ IntegerTypesAndOperations/ReadingIntegerNumbers/ReadingIntegerNumbersOutput/CMakeFiles/ReadingIntegerNumbers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReadingIntegerNumbers.dir/ReadingIntegerNumbers.cpp.obj"
+  "ReadingIntegerNumbers.exe"
+  "ReadingIntegerNumbers.exe.manifest"
+  "ReadingIntegerNumbers.pdb"
+  "libReadingIntegerNumbers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReadingIntegerNumbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
