376467c7ba79973599ab47ce9c55c7abff7f7291
anmol <anmolsoin1@gmail.com>
2022-01-30 20:05:57 +0530

    ProjectWork

---
+++ IntegerTypesAndOperations/GoodRestOnVacations/GoodRestOnVacationsOutput/CMakeFiles/GoodRestOnVacations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GoodRestOnVacations.dir/GoodRestOnVacations.cpp.obj"
+  "GoodRestOnVacations.exe"
+  "GoodRestOnVacations.exe.manifest"
+  "GoodRestOnVacations.pdb"
+  "libGoodRestOnVacations.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GoodRestOnVacations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
